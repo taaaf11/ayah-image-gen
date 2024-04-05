035aceaff645b7edc4025ce967fc33b02a48231d
tofi1130 <taafuuu@gmail.com>
2024-04-05 23:44:51 +0500

    initial commit

---
+++ lib/surah_name_box.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SurahNameBox extends StatefulWidget {
+  const SurahNameBox({super.key});
+
+  @override
+  State<SurahNameBox> createState() => _SurahNameBoxState();
+}
+
+class _SurahNameBoxState extends State<SurahNameBox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
